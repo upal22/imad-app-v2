63d4b3a99b689b5f525bc2bceff4222a102b8855
upal22 <upalchatt@gmail.com>
2017-02-16 18:46:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,10 @@
 console.log('Loaded!');
 var img= document.getElementById('pallu');
+var marginleft=0;
+function moveRight(){
+    marginleft= marginleft+10;
+    img.style.marginleft= marginleft+'px';
+}
 img.onclick=function(){
-img.style.marginleft= '100px';    
+var interval=setInterval(moveRight,100);    
 };
